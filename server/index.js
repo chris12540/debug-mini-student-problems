2049847261055bee826bedf513d394fdf4b5ed79
Christian Ramos <chris12540@gmail.com>
2018-10-23 19:08:08 -0700

    prob3

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/data', (req, res) => {
-  res.json({ greeting: 'hello' });
+app.get("/data", (req, res) => {
+	res.json({ greeting: "hello" });
 });
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
 app.listen(SERVER_PORT, () => {
-  console.log('Server listening on port ' + SERVER_PORT);
+	console.log("Server listening on port " + SERVER_PORT);
 });
